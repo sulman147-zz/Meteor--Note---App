d96e44ebed58473471d8213374f3b1d9eadf9d27
Muhammad Sulman <msulman147@gmail.com>
2017-10-19 17:56:18 +0500

    new note updated

--- imports/UI/NoteListHeader.js
+++ imports/UI/NoteListHeader.js
@@ -2,23 +2,30 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-
+import { Session } from 'meteor/session';
 export const NoteListHeader = (props) => {
   return (
     <div>
       <button onClick={() => {
-        props.meteorCall('notes.insert');
+        props.meteorCall('notes.insert',(err, res) => {
+          if (res) {
+            props.Session.set('selectedNoteId', res);
+          }
+
+        });
       }}>Create Note</button>
     </div>
   );
 };
 
 NoteListHeader.propTypes = {
-  meteorCall: PropTypes.func.isRequired
+  meteorCall: PropTypes.func.isRequired,
+  Session: PropTypes.object.isRequired
 };
 
 export default createContainer(() => {
   return {
-    meteorCall: Meteor.call
+    meteorCall: Meteor.call,
+    Session
   };
 }, NoteListHeader);
